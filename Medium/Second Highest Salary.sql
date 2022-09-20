8fa87ece3c1a67319f28c38f38172bf425661656
Seif Reda <69864768+SeifReda30@users.noreply.github.com>
2022-09-20 18:35:35 +0200

    Update Second Highest Salary.sql

--- Medium/Second Highest Salary.sql
+++ Medium/Second Highest Salary.sql
@@ -9,7 +9,7 @@ select case when (select count(*) from (select distinct salary from employee) as
 /*
 In this Problem we should get the second highest salary and it looks easy but the code will expose to test cases which will output null
 
-1- The Output may be Null which indicate that there is no second highest salary and it is the case which the table contains only one salary, and I solved this case by counting the total number of distinct salaries in employee table and if there is only one row then it is null
+1- The Output will be Null which indicate that there is no second highest salary and it is the case which the table contains only one salary, and I solved this case by counting the total number of distinct salaries in employee table and if there is only one row then it is null
 
 2- If there are more than one salary so we can get the second highest salary by selecting the top 2 distinct salaries from the table ordered by the salary value desc, and then taking the minimum salary from the top 2 highest salaries and that will give us the second highest salary
 
